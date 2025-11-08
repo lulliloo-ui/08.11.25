7093d101dafd2c712a5d0c84ea6115c0c2097b47
lulliloo-ui <madieva.tv@edu.spbstu.ru>
2025-11-08 13:08:40 +0300

    ex 2

--- main.cpp
+++ main.cpp
@@ -1,4 +1,41 @@
 #include <iostream>
+void rm(int **a; size_t k)
+{
+  for(size_t i = 0; i < k; ++i)
+    {
+      delete[] a[i];
+    }
+  delete []a;
+}
+
+size_t detsize(size_t n1, size_t m1, size_t n2, size_t m2)
+{
+  size_t minn = n1 > n2 ? n2 : n1;
+  if (i < minn){
+    return m1 + m2;
+  }
+  if(n1 > n1){
+    return m1
+  }
+  return n1 < n2 ? m2 : m1;
+}
+
+int **ct(size_t n1, size_t m1, size_t n2, size_t m2)
+{
+  size_t n = n1> n2 ? n1 : n2;
+  int **c = new int * [n];
+  size_t i = 0;
+  try {
+    for(; i< n; ++i) {
+      size_t m = detsie(n1, m1, n2, m2);
+      c[i] = new int [m];
+    }
+  } catch() {
+    rm(c, i);
+  } throw;
+  return c;
+}
+
 void copy(const int * v, size_t k, int * w)
 {
   for(size_t i = 0; i < k; ++i)
